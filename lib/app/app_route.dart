bbe3c8d522dbdb4b927b6cb6d7f01672de0f7a77
willyyypatootieee <blueplayz26@gmail.com>
2026-03-29 09:42:21 +0700

    feat: add home route to AppRoutes for navigation to MainScreen

--- lib/app/app_route.dart
+++ lib/app/app_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../features/auth/presentation/auth_screen.dart';
+import '../features/main/presentation/main_screen.dart';
 import '../features/onboarding/presentation/onboarding_screen.dart';
 import '../features/splash/presentation/splash_screen.dart';
 
@@ -7,10 +8,12 @@ class AppRoutes {
   static const splash = '/';
   static const onboarding = '/onboarding';
   static const auth = '/auth';
+  static const home = '/home';
 
   static Map<String, WidgetBuilder> get map => {
     splash: (_) => const SplashScreen(),
     onboarding: (_) => const OnboardingScreen(),
     auth: (_) => const AuthScreen(),
+    home: (_) => const MainScreen(),
   };
 }
