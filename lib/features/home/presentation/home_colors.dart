37e9895b048bf0a51c82391ebf834afa9bf51c91
willyyypatootieee <blueplayz26@gmail.com>
2026-03-29 09:30:36 +0700

    feat: add HomeColors class with color constants for UI elements

---
+++ lib/features/home/presentation/home_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeColors {
+  static const Color background = Color(0xFFF7F9FC);
+  static const Color headerBlue = Color(0xFF1EADEF);
+
+  static const Color statusGreen = Color(0xFFE8FAF6);
+  static const Color statusGreenText = Color(0xFF00BFA5);
+
+  // Service Icon Colors
+  static const Color iconBgClean = Color(0xFFE8F5E9);
+  static const Color iconColorClean = Color(0xFF00BFA5);
+  static const Color iconBgService = Color(0xFFE3F2FD);
+  static const Color iconColorService = Color(0xFF1EADEF);
+  static const Color iconBgPasang = Color(0xFFFFEBEE);
+  static const Color iconColorPasang = Color(0xFFE53935);
+  static const Color iconBgCopot = Color(0xFFE0F2F1);
+  static const Color iconColorCopot = Color(0xFF26A69A);
+  static const Color iconBgInspect = Color(0xFFFFEBEE);
+  static const Color iconColorInspect = Color(0xFFE53935);
+
+  static const Color badgeOrange = Color(0xFFFF6D00);
+}
