5c06e27e3485e42aed3705105e0d1397a4b3b3fc
willyyypatootieee <blueplayz26@gmail.com>
2026-04-03 11:38:21 +0700

    feat: add DashedDivider widget for horizontal dashed line UI

---
+++ lib/features/history/presentation/widgets/order_detail/dashed_divider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class DashedDivider extends StatelessWidget {
+  const DashedDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final boxWidth = constraints.constrainWidth();
+        const dashWidth = 5.0;
+        const dashHeight = 1.0;
+        final dashCount = (boxWidth / (2 * dashWidth)).floor();
+        return Flex(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          direction: Axis.horizontal,
+          children: List.generate(dashCount, (_) {
+            return SizedBox(
+              width: dashWidth,
+              height: dashHeight,
+              child: DecoratedBox(
+                decoration: BoxDecoration(color: Colors.grey.shade300),
+              ),
+            );
+          }),
+        );
+      },
+    );
+  }
+}
